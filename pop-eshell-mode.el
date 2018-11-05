1cab6e79de5b49b73f0e01f21c6adcdb04429e53
stanhe <hshl4314@gmail.com>
2018-11-05 09:40:50 +0800

    improve docs

--- pop-eshell-mode.el
+++ pop-eshell-mode.el
@@ -3,6 +3,8 @@
 ;; author: stanhe
 ;; date: 2018-10-27
 
+;;;; Usage:
+
 ;; just bind fast-eshell-pop and eshell-pop-toggle to your key-bindings
 ;;
 ;; eshell-pop-toggle:
@@ -10,36 +12,53 @@
 ;; fast-eshell-pop:
 ;;      open eshell with project mode,open git or gradle project.
 
+;;;; Change log:
 
-;;define 
+;; 2018/11/5
+;;     * Improve docs.
+;;
+;; 2018/11/1
+;;     * Expose var for search parent directory,see `pop-find-parent-directory'.
+;;
+;; 2018/10/31
+;;     * Add feature mult windows support,jump between mult windows.
+;;     * Add prefix arg to full screen eshell to locate current file directory.
+;;
+;; 2018/10/31
+;;     * First released.
+
+
+
+;;define var
 (defvar my-eshell " *BOTTOM-TERMINAL*" "my shell name,use eshell.")
 (defvar my-full-eshell " *FULL-TERMINAL*" "my full shell name,use eshell.")
 
 (defvar pop-find-parent-directory nil "find the files in parent directory,if find,return the path as parent directory,else try the next")
 
-(defvar pre-buffer nil "previous used buffer.")
-(defvar pre-path nil "previous open directory.")
+(defvar pre-buffer nil "previous buffer.")
+(defvar pre-path nil "previous directory.")
 (defvar pre-parent-path nil "previous parent directory.")
 
-;;function
+;;functions
 (defun get-current-directory (&optional buffer)
   "get current directory."
-  (if (not buffer)
-      (file-name-directory (or (buffer-file-name) default-directory))
-    (with-current-buffer buffer
-      (file-name-directory (or (buffer-file-name) default-directory)))))
+  (if buffer
+      (with-current-buffer buffer
+	(file-name-directory (or (buffer-file-name) default-directory)))
+    (file-name-directory (or (buffer-file-name) default-directory))))
 
 (defun get-parent-dir (name)
   "Get the parent name dir."
   (locate-dominating-file default-directory name))
 
 (defun get-project-root-directory (buffer)
-  "find current project root,for git or gradle."
+  "find current project root,set by `pop-find-parent-directory',
+this function will check the parent directory for special file in order. "
     (with-current-buffer buffer
       (if (setq parent (cl-some #'get-parent-dir pop-find-parent-directory))
 	  parent
 	(get-current-directory))))
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; side window bottom ;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun eshell-pop-bottom()
   "pop eshell at bottom"
   (interactive)
@@ -62,9 +81,12 @@
       (eshell-send-input)
       (setq pre-path dir))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; full window ;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;###autoload
 (defun fast-eshell-pop (&optional pre-arg)
-  "fast jump to eshll,it's the same as M-x :eshell "
+  "fast jump between current buffer and full screen eshell buffer,
+with any pre-arg locate to current buffer directory,
+otherwise to the parent directory,set by `pop-find-parent-directory' "
   (interactive "p")
   (let* ((buffer (current-buffer))
 	 (shell (get-buffer my-full-eshell))
@@ -88,12 +110,10 @@
 	    (select-window exist-window)
 	  (switch-to-buffer shell))
 	(setq pre-buffer buffer)
-	(progn
-	  (when (not (equal pre-parent-path dir))
-	    (eshell/cd dir)
-	    (eshell-send-input)
-	    (setq pre-parent-path dir)))))
-    (bury-buffer shell)))
+	(when (not (equal pre-parent-path dir))
+	  (eshell/cd dir)
+	  (eshell-send-input)
+	  (setq pre-parent-path dir))))))
 
 ;; keymaps
 (defvar pop-eshell-mode-map
@@ -105,7 +125,7 @@
 
 ;;;###autoload
 (defun eshell-pop-toggle ()
-  "pop eshell or hide."
+  "pop or hide bottom eshell side window."
   (interactive)
   (if (get-buffer-window my-eshell)
       (delete-windows-on my-eshell)
